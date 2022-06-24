af8756585f21bb0b8e69118053a4a001a8153878
Zidea <a827569457@163.com>
2022-06-24 17:05:49 +0800

    now world can generate Ruam ore

---
+++ Common/Systems/Genpasses/RuamOreGenPasses.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+using GreatForger.Content.Tiles;
+
+namespace GreatForger.Common.Systems.Genpasses
+{
+    internal class RuamOreGenPasses : GenPass
+    {
+        public RuamOreGenPasses(string name, float weight): base(name, weight) { }
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            progress.Message = "Spawning Ruam Ores";
+
+            // RuamOre
+            int maxToSpawn = (int)(Main.maxTilesX * Main.maxTilesY* 6E-05);
+            for (int i =0;i<maxToSpawn;i++)
+            {
+                int x = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
+                int y = WorldGen.genRand.Next((int)WorldGen.worldSurface, Main.maxTilesY - 300);
+                WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 5), ModContent.TileType<RuamOre>());
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ Common/Systems/WorldSystem.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+using System.Collections.Generic;
+using GreatForger.Common.Systems.Genpasses;
+
+namespace GreatForger.Common.Systems
+{
+    internal class WorldSystem : ModSystem
+    {
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int shiniesIndex = tasks.FindIndex(t => t.Name.Equals("Shinies"));
+            if (shiniesIndex != -1)
+            {
+                tasks.Insert(shiniesIndex + 1, new RuamOreGenPasses("Ruam Ore Pass", 320f));
+            }
+        }
+    }
+}
